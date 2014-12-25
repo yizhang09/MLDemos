5c74bd097250b3cbd01d512916b4867fe2c0d19c
ZhangYi <yizhang09@gmail.com>
2014-12-25 16:19:44 +0800

    new

---
+++ python/movielensrec.py
@@ -0,0 +1,25 @@
+__author__ = 'zhangyi'
+
+def loadMovielens(path='data/movielens/'):
+    movies={}
+    for line in open(path+'u.item'):
+        (id,title)=line.split('|')[0:2]
+        movies[id]=title
+
+
+    prefs={}
+    for line in open(path+'u.data'):
+        (user,movieid,rating,ts)=line.split('\t')
+        prefs.setdefault(user,{})
+        prefs[user][movies[movieid]]=float(rating)
+
+    return prefs
+
+
+import recom
+prefs = loadMovielens()
+result = recom.getRecommendations(prefs,'87')[0:30]
+
+print result
+
+
