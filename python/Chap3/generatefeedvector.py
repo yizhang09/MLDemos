5431475ccd8585ca6f43bed783f3740a305200d0
ZhangYi <yizhang09@gmail.com>
2015-02-06 09:33:27 +0800

    add cluster chap3

--- python/Chap3/generatefeedvector.py
+++ python/Chap3/generatefeedvector.py
@@ -5,7 +5,7 @@ import feedparser
 import re
 
 
-#获取rss feed 单词计数
+# 获取rss feed 单词计数
 def getwordcounts(url):
     try:
         d = feedparser.parse(url)
@@ -14,7 +14,9 @@ def getwordcounts(url):
 
     wc = {}
 
-    if d.entries == {}:
+    print(len(d.entries))
+
+    if len(d.entries) == 0:
         return
 
     for e in d.entries:
@@ -42,14 +44,20 @@ def getwords(html):
     return [word.lower() for word in words if word != '']
 
 
+#全局单词计数
 apcount = {}
+#每个feedurl对应的单词计数
 wordcounts = {}
+
+feedlist = [line for line in file('feedlist.txt')]
 
 for feedurl in file('feedlist.txt'):
     try:
         print(feedurl)
         title, wc = getwordcounts(feedurl)
-        print(title)
+        if(title == ''):
+            continue
+        print(title+'\n')
     except Exception:
         continue
     wordcounts[title] = wc
@@ -57,6 +65,25 @@ for feedurl in file('feedlist.txt'):
         apcount.setdefault(word, 0)
         if count > 1:
             apcount[word] += 1
+
+wordlist = []
+for w, bc in apcount.items():
+    frac = float(bc) / len(feedlist)
+    if (frac > 0.1 and frac < 0.5):
+        wordlist.append(w)
+
+out = file("blogdata.txt", "w")
+out.write("Blog")
+for word in wordlist: out.write('\t%s', word)
+out.write('\n')
+for blog, wc in wordcounts.items():
+    out.write(blog)
+    for word in wordlist:
+        if word in wc:
+            out.write('\t%d' % word)
+        else:
+            out.write('\t0')
+    out.write('\n')
 
 #for w in getwordcounts('http://feeds.bbci.co.uk/news/rss.xml'):
 #    print(w)
