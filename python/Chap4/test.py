290036925eb8bbbe2f9d9e7c27717b411acb4355
ZhangYi <yizhang09@gmail.com>
2015-03-14 17:35:03 +0800

    add searchengine code Signed-off-by: ZhangYi <yizhang09@gmail.com>

--- python/Chap4/test.py
+++ python/Chap4/test.py
@@ -1,8 +1,25 @@
+# coding:utf-8
+
 __author__ = 'zhangyi'
 
-from searchengine import searchengine
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-pagelist = ['http://www.cnblogs.com/']
-crawler = searchengine.crawler('')
-crawler.crawl(pagelist)
+import searchengine
+
+#pagelist = ['http://www.cnblogs.com/']
+#c = searchengine.crawler('se.db')
+#crawler.crawl(pagelist)
+
+#c.con.execute("insert into urllist(url,title) values ('www.cnblogs.com','博客园')")
+#rows = [row for row in c.con.execute("select * from urllist")]
+
+#print(rows)
+
+s = '博客园'
+
+print(s)
+#crawler.createindextables()
+
 
