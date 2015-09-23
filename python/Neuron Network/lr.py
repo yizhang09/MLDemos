c19c99593209f8e3b78b264c9117870de7b2a85c
ZhangYi <yizhang09@gmail.com>
2015-09-23 16:07:57 +0800

    add nn.py an implement of neuro network with tanh as activition func.

---
+++ python/Neuron Network/lr.py
@@ -0,0 +1,44 @@
+import numpy as np
+import sklearn as sk
+import sklearn.datasets
+import sklearn.linear_model
+import matplotlib.pyplot as plt
+import matplotlib
+
+#
+
+matplotlib.rcParams['figure.figsize'] = (10.0,8.0)
+
+np.random.seed(0)
+X,y = sk.datasets.make_moons(200,noise=0.20)
+#plt.scatter(X[:,0],X[:,1],s=40,c=y,cmap=plt.spectral)
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+#plt.show()
+
+clf = sklearn.linear_model.LogisticRegression()
+clf.fit(X,y)
+x_min,x_max = X[:,0].min() - .5,X[:,0].max() + .5
+y_min,y_max = X[:,1].min() - .5,X[:,1].max() + .5
+h = 0.01
+xx,yy = np.meshgrid(np.arange(x_min,x_max,h),np.arange(y_min,y_max,h))
+Z = clf.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx,yy,Z,cmap=plt.cm.Spectral)
+plt.scatter(X[:,0],X[:,1],c=y,cmap=plt.cm.Spectral)
+plt.show()
+
+
+def plot_decision_boundary(pred_func):
+   x_min,x_max = X[:,0].min() - .5,X[:,0].max() + .5
+   y_min,y_max = X[:,1].min() - .5,X[:,1].max() + .5
+   h = 0.01
+   xx,yy = np.meshgrid(np.arange(x_min,x_max,h),np.arange(y_min,y_max,h))
+   Z = pred_func(np.c_[xx.ravel(),yy.ravel()])
+   Z = Z.reshape(xx.shape)
+   plt.contourf(xx,yy,Z,cmap=plt.cm.Spectral)
+   plt.scatter(X[:,0],X[:,1],c=y,cmap=plt.cm.Spectral)
+   plt.show()
+
+#plot_decision_boundary(lambda x:clf.predict(x))
+
+
